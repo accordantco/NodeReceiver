5c5cf8288c3c699299f33693bc78ffdfbbdde0c6
Jedediah Arnold <jarnold@accordantco.com>
2024-03-14 17:01:53 -0400

    save blob

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+import { put } from "@vercel/blob";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Endpoint to catch all requests and save them to a file
-app.all('/save-request', (req, res) => {
+app.all('/save-request', async (req, res) => {
     const requestData = `
         Method: ${req.method}
         URL: ${req.protocol}://${req.get('host')}${req.originalUrl}
@@ -20,7 +21,8 @@ app.all('/save-request', (req, res) => {
         Body: ${JSON.stringify(req.body, null, 2)}
     `;
 
-    res.send(requestData);
+    const { url } = await put('requests/blob.txt', requestData, { access: 'public' });
+    res.send(url);
 });
 
 // Start the server
