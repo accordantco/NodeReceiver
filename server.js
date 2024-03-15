ca5a0a1c53de2edee90472d0461dcb9042be0a05
Jedediah Arnold <jarnold@accordantco.com>
2024-03-15 08:54:46 -0400

    let's try again

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Endpoint to catch all requests and save them to a file
-app.all('/save-request', async (req, res) => {
+app.post('/save-request', async (req, res) => {
     const requestData = `
         Method: ${req.method}
         URL: ${req.protocol}://${req.get('host')}${req.originalUrl}
         Headers: ${JSON.stringify(req.headers, null, 2)}
         Body: ${JSON.stringify(req.body, null, 2)}
     `;
-    const { url } = await put('articles/blob.txt', requestData, { access: 'public', token: "vercel_blob_rw_Vr8jWMq7Dmphgbz7_WEjsirInc1SmLQFfCYstlTjz4QVNn2" });
+    await put('articles/blob.txt', requestData, { access: 'public' });
     res.send(requestData);
 });
 
